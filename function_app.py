f25d9f0293cdc4224efe924999d526388a885822
mikael <mikael.ahola@skillio.ai>
2025-01-07 08:49:21 +0200

    did changes

--- function_app.py
+++ function_app.py
@@ -1,18 +1,18 @@
 import logging
 import azure.functions as func
-from test1 import process_pptx_files 
+from test1.test import process_pptx_files  
 
-app = func.FunctionApp()
+app = func.FunctionApp() 
 
 @app.timer_trigger(schedule="0 10 * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def testtimer(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
-
+    
     logging.info('Timer trigger function executed.')
 
     try:
-
-        process_pptx_files()
+        process_pptx_files()  
     except Exception as e:
         logging.error(f"Error executing pptx processing: {str(e)}")
+
